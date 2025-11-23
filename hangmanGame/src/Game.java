e21657b97fce5dd1de13494afe4306c15be6f145
Annamalai <an.nachiappan06@gmail.com>
2025-11-23 11:52:12 +0000

    Basic plan of how Game class will work

---
+++ hangmanGame/src/Game.java
@@ -0,0 +1,17 @@
+public class Game {
+    private int attempt;
+    public static final int drawCharacters = 8;
+    public Game(){
+        attempt = 0;
+        while (attempt < drawCharacters){
+            //l = UI.getInput()
+            //ind = word.contains(l)
+            //if(ind != -1){
+            // UI.printOut(ind)
+            // }else{
+            // UI.printHang(attempt)
+            // }
+            attempt += 1;
+        }
+    }
+}
