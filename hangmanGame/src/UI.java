2499141f660841cd76c1cf4e763df587300dd229
Annamalai <an.nachiappan06@gmail.com>
2025-11-23 12:50:08 +0000

    Adding input and output procedures to UI

--- hangmanGame/src/UI.java
+++ hangmanGame/src/UI.java
@@ -1,2 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class UI {
+    public static final String[] stages = {"H","HA","HAN","HANG","HANGM", "HANGMA","HANGMAN"};
+    private BufferedReader br;
+    public UI(){
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+    public static void printHangStage(int index){
+        System.out.println(stages[index]);
+    }
+    public String getGuess(int index) {
+        System.out.println("Give me your guess. Enter only one character.");
+        boolean done = false;
+        String s = "";
+        while (!done){
+            done = true;
+            try{
+                s = br.readLine();
+                if(s.length() != 1){
+                    System.out.println("Please enter one character");
+                    done = false;
+                }
+            }catch(IOException e){
+                done = false;
+                System.out.println("Input problem. Try again.");
+            }
+        }
+        return s;
+    }
 }
