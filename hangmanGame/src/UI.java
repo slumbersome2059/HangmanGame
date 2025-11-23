806f8122a57821a11ec01b19d767ca495ba27e35
Annamalai <an.nachiappan06@gmail.com>
2025-11-23 14:03:15 +0000

    Adding method to give output with strings found

--- hangmanGame/src/UI.java
+++ hangmanGame/src/UI.java
@@ -1,18 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class UI {
     public static final String[] stages = {"H","HA","HAN","HANG","HANGM", "HANGMA","HANGMAN"};
     private BufferedReader br;
-    public UI(){
+    private StringBuilder out;
+    public UI(int wordLength){
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] t = new char[wordLength];
+        Arrays.fill(t, '_');
+        out = new StringBuilder(new String(t));
     }
     public static void printHangStage(int index){
         System.out.println(stages[index]);
     }
-    public String getGuess(int index) {
+    public char getGuess(int index) {
         System.out.println("Give me your guess. Enter only one character.");
         boolean done = false;
         String s = "";
@@ -29,6 +34,10 @@ public class UI {
                 System.out.println("Input problem. Try again.");
             }
         }
-        return s;
+        return s.toLowerCase().charAt(0);
+    }
+    public void printOut(int index, char letter){
+        out.setCharAt(index, letter);
+        System.out.println(out);
     }
 }
